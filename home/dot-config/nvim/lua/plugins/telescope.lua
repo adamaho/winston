ae5631daf97cf246a358c7c0aa39423d94ecaf0f
Adam Aho <aho.adam01@gmail.com>
2025-07-15 02:00:01 +0000

    update tmux config and telescope

--- home/dot-config/nvim/lua/plugins/telescope.lua
+++ home/dot-config/nvim/lua/plugins/telescope.lua
@@ -21,8 +21,9 @@ return {
 
     -- keymaps 
     vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "find all files" })
+    vim.keymap.set("n", "<leader>fh", function() builtin.find_files({ hidden = true }) end, { desc = "find all files including hidden files" })
     vim.keymap.set("n", "<leader>fi", builtin.live_grep, { desc = "find in files" })
     vim.keymap.set("n", "<leader>fd", builtin.diagnostics, { desc = "find diagnostics in workspace" })
-    vim.keymap.set("n", "<leader>fb", function() builtin.diagnostics({bufnr=0}) end, { desc = "find diagnostics in buffer" })
+    vim.keymap.set("n", "<leader>fb", function() builtin.diagnostics({bufnr=0}) end, { desc = "find diagnostics in current buffer" })
   end,
 }
