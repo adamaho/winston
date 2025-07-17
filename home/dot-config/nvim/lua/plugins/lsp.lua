6ae1e684a454bbe07a0a0af806b7080f54c37be3
Adam Aho <aho.adam01@gmail.com>
2025-07-17 02:41:08 +0000

    formatting plugin

--- home/dot-config/nvim/lua/plugins/lsp.lua
+++ home/dot-config/nvim/lua/plugins/lsp.lua
@@ -1,36 +1,50 @@
 return {
-  "neovim/nvim-lspconfig",
-  dependencies = {
-    "mason-org/mason.nvim",
-    "mason-org/mason-lspconfig.nvim",
-    "hrsh7th/nvim-cmp",
-  },
-  config = function()
-    -- lsp
-    require("mason").setup()
-    require("mason-lspconfig").setup({
-      ensure_installed = { "lua_ls" },
-    })
+	"neovim/nvim-lspconfig",
+	dependencies = {
+		"mason-org/mason.nvim",
+		"mason-org/mason-lspconfig.nvim",
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+		"hrsh7th/nvim-cmp",
+	},
+	config = function()
+		-- lsp
+		require("mason").setup()
+		require("mason-lspconfig").setup({
+			ensure_installed = { "lua_ls" },
+		})
+		require("mason-tool-installer").setup({
+			ensure_installed = { "stylua" },
+		})
 
-    local lspconfig = require("lspconfig")
-    local capabilities = require("cmp_nvim_lsp").default_capabilities()
+		local lspconfig = require("lspconfig")
+		local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-    local on_attach = function(client, bufnr)
-      vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
+		local on_attach = function(client, bufnr)
+			vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
 
-      -- keymaps
-      local opts = { noremap = true, silent = true, buffer = bufnr }
+			-- keymaps
+			local opts = { noremap = true, silent = true, buffer = bufnr }
 
-      vim.keymap.set("n", "gd", vim.lsp.buf.definition, vim.tbl_extend("force", opts, { desc = "go to definition" }))
-      vim.keymap.set("n", "K", vim.lsp.buf.hover, vim.tbl_extend("force", opts, { desc = "hover documentation" }))
-      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, vim.tbl_extend("force", opts, { desc = "open code actions" }))
-    end
+			vim.keymap.set(
+				"n",
+				"gd",
+				vim.lsp.buf.definition,
+				vim.tbl_extend("force", opts, { desc = "go to definition" })
+			)
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, vim.tbl_extend("force", opts, { desc = "hover documentation" }))
+			vim.keymap.set(
+				"n",
+				"<leader>ca",
+				vim.lsp.buf.code_action,
+				vim.tbl_extend("force", opts, { desc = "open code actions" })
+			)
+		end
 
-    for _, server in ipairs(require("mason-lspconfig").get_installed_servers()) do
-      lspconfig[server].setup({
-        on_attach = on_attach,
-        capabilities = capabilities,
-      })
-    end
-  end,
+		for _, server in ipairs(require("mason-lspconfig").get_installed_servers()) do
+			lspconfig[server].setup({
+				on_attach = on_attach,
+				capabilities = capabilities,
+			})
+		end
+	end,
 }
--- home/dot-config/nvim/lua/plugins/telescope.lua
+++ home/dot-config/nvim/lua/plugins/telescope.lua
@@ -19,13 +19,16 @@ return {
 
     telescope.load_extension("fzf")
 
-    -- keymaps 
+    -- keymaps
     local opts = { noremap = true, silent = true }
 
     vim.keymap.set("n", "<leader>ff", builtin.find_files, vim.tbl_extend("force", opts, { desc = "find all files" }))
-    vim.keymap.set("n", "<leader>fh", function() builtin.find_files({ hidden = true }) end, vim.tbl_extend("force", opts, { desc = "find all files including hidden files" }))
+    vim.keymap.set("n", "<leader>fh", function() builtin.find_files({ hidden = true }) end,
+      vim.tbl_extend("force", opts, { desc = "find all files including hidden files" }))
     vim.keymap.set("n", "<leader>fi", builtin.live_grep, vim.tbl_extend("force", opts, { desc = "find in files" }))
-    vim.keymap.set("n", "<leader>fd", builtin.diagnostics, vim.tbl_extend("force", opts, { desc = "find diagnostics in workspace" }))
-    vim.keymap.set("n", "<leader>fb", function() builtin.diagnostics({bufnr=0}) end, vim.tbl_extend("force", opts, { desc = "find diagnostics in current buffer" }))
+    vim.keymap.set("n", "<leader>fd", builtin.diagnostics,
+      vim.tbl_extend("force", opts, { desc = "find diagnostics in workspace" }))
+    vim.keymap.set("n", "<leader>fb", function() builtin.diagnostics({ bufnr = 0 }) end,
+      vim.tbl_extend("force", opts, { desc = "find diagnostics in current buffer" }))
   end,
 }
