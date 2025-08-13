07667d1dd20a1de05f724e9e656da278c8d90910
Adam Aho <aho.adam01@gmail.com>
2025-08-13 00:08:39 +0000

    update keymaps

--- home/dot-config/nvim/lua/config/keymaps.lua
+++ home/dot-config/nvim/lua/config/keymaps.lua
@@ -4,18 +4,21 @@ vim.g.mapleader = " "
 
 -- stylua: ignore start
 vim.keymap.set('n', '<Esc>', ':nohl<CR>')
+vim.keymap.set("i", "jj", "<Esc>", vim.tbl_extend("force", opts, { desc = "exit insert mode" }))
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", vim.tbl_extend("force", opts, { desc = "move lines down" }))
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", vim.tbl_extend("force", opts, { desc = "move lines up" }))
 
-vim.keymap.set("i", "jj", "<Esc>", vim.tbl_extend("force", opts, { desc = "exit insert mode" }))
+-- lines 
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", vim.tbl_extend("force", opts, { desc = "Move block down" }))
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", vim.tbl_extend("force", opts, { desc = "Move block up" }))
 
-vim.keymap.set("n", "j", "gj", vim.tbl_extend("force", opts, { desc = "move down by display line" }))
-vim.keymap.set("n", "k", "gk", vim.tbl_extend("force", opts, { desc = "move up by display line" }))
-vim.keymap.set("n", "<Down>", "gj", vim.tbl_extend("force", opts, { desc = "move down by display line" }))
-vim.keymap.set("n", "<Up>", "gk", vim.tbl_extend("force", opts, { desc = "move up by display line" }))
-
-vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float, vim.tbl_extend("force", opts, { desc = "view line diagnostics" }))
+vim.keymap.set("n", "<leader>cd", vim.diagnostic.open_float, vim.tbl_extend("force", opts, { desc = "view line diagnostics" }))
 vim.keymap.set("n", "<leader>[d", vim.diagnostic.goto_prev, vim.tbl_extend("force", opts, { desc = "go to previous diagnostic" }))
 vim.keymap.set("n", "<leader>]d", vim.diagnostic.goto_next, vim.tbl_extend("force", opts, { desc = "go to next diagnostic" }))
+
+-- buffers
+vim.keymap.set("n", "<leader>bd", ":bd<CR>", vim.tbl_extend("force", opts, { desc = "Close buffer" }))
+vim.keymap.set("n", "<S-l>", ":bn<CR>", vim.tbl_extend("force", opts, { desc = "Next buffer" }))
+vim.keymap.set("n", "<S-h>", ":bp<CR>", vim.tbl_extend("force", opts, { desc = "Previous buffer" }))
 -- stylua: ignore end
