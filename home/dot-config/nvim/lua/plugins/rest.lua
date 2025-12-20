739e535a8bed4b6451ad57e8c480f33452c0fef7
Adam Aho <aho.adam01@gmail.com>
2025-12-20 00:58:37 +0000

    add shortcut

--- home/dot-config/nvim/lua/plugins/rest.lua
+++ home/dot-config/nvim/lua/plugins/rest.lua
@@ -20,11 +20,26 @@ return {
 		},
 	},
 	config = function()
+		-- Setup rest.nvim with response window below
+		require("rest-nvim").setup({
+			result = {
+				split = "horizontal", -- Opens response below instead of to the left
+			},
+		})
+
 		-- Configure JSON formatting for rest.nvim responses
 		vim.api.nvim_create_autocmd("FileType", {
 			pattern = "json",
 			callback = function()
 				vim.bo.formatprg = "jq"
+			end,
+		})
+
+		-- Add keymaps for http files
+		vim.api.nvim_create_autocmd("FileType", {
+			pattern = "http",
+			callback = function()
+				vim.keymap.set("n", "<leader>rr", "<cmd>Rest run<cr>", { buffer = true, desc = "Run request" })
 			end,
 		})
 	end,
